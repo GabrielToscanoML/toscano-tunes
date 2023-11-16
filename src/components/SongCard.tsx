6c457624cc245b7fef61975458e5da5faca07f03
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-11-16 10:24:44 -0300

    Estilização do checkbox feita via arquivo CSS

--- src/components/SongCard.tsx
+++ src/components/SongCard.tsx
@@ -2,6 +2,11 @@ import { addSong, removeSong } from "@/app/utils/localStorage/favoriteSongs";
 import { Song } from "@/types/types";
 import { useState } from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+import './checkbox.css';
+
 export function SongCard(props: Song) {
   const [favorited, setFavorited] = useState(false);
 
@@ -19,20 +24,27 @@ export function SongCard(props: Song) {
     return(
       <main className="flex flex-col justify-center">
         <h1 className="mb-2 font-medium">{props.trackName}</h1>
-        <div>
+        <div className="flex items-center">
           <audio className="" src={props.previewUrl} controls>
               <track kind="captions" />
               O seu navegador não suporta o elemento{props.trackName} <code>audio</code>.
           </audio>
-          <label htmlFor={ props.trackId }>
+          <label
+            htmlFor={ props.trackId }
+            className="heart-checkbox"
+          >
             <input
+              className="hidden"
               type="checkbox"
               id={ props.trackId }
               value={ JSON.stringify(props) } // usando JSON pra transformar em objeto
               checked={ favorited }
               onChange={onChangeCheckBox}
             />
-            Favorita
+            <FontAwesomeIcon
+              icon={faHeart}
+              className="heart-icon"
+            />
           </label>
         </div>
       </main>
