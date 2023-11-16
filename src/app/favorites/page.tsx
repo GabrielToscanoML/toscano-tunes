1f429402562a6e9b46877e2dc417500042722712
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-11-16 10:34:30 -0300

    [WIP] :construction: Página de favoritos em progresso. Preciso arrumar o checkbox na pagina

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import { Header } from "@/components/Header";
+import { useEffect, useState } from "react";
+import { getFavoriteSongs } from "../utils/localStorage/favoriteSongs";
+import { Song } from "@/types/types";
+import { SongCard } from "@/components/SongCard";
 
 export default function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const {songs} = getFavoriteSongs();
+    setFavorites(songs);
+    console.log('favorites ', favorites);
+  }, [])
+
   return (
     <main>
       <Header />
+      <div>
+        {
+          favorites.map((song: Song) => {
+            return(
+              <div key={song.trackId}>
+                <SongCard
+                    trackName={song.trackName}
+                    previewUrl={song.previewUrl}
+                    trackId={song.trackId}
+                />
+              </div>
+            )
+          })
+        }
+      </div>
     </main>
   )
 }
